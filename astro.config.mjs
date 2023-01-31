ffaea8ce0a9081a911e79cb6e3e3c70bcb582441
smetzdev <hello@smetz.dev>
2023-01-31 13:21:02 +0100

    refactor(smetz.dev): :heavy_plus_sign: add @astro/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
-import { defineConfig } from "astro/config"
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind"
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-  ],
-})
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), image()]
+});
