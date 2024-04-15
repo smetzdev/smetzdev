4bfce8f74bd92f8df3bbf71110e117e803244062
Sascha Metz <hello@smetz.dev>
2024-04-15 15:04:55 +0200

    refactor: :lipstick: use brawn style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        brawn: {
+          white: "#FEFFFF",
+          gray: "#20303F",
+          yellow: "#B8FD6E",
+          black: "#142127",
+          red: "#BD2E40",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
