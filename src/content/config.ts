a43ef49879a659ed8264fbdb2ddec309dbf97989
Sascha Metz <hello@smetz.dev>
2024-08-10 15:46:16 +0200

    feat: :sparkles: add projects and footer

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// 2. Define your collection(s)
+const projectCollection = defineCollection({
+  type: "data",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    href: z.string(),
+    designerName: z.string().optional(),
+    designerLink: z.string().optional(),
+  }),
+});
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  projects: projectCollection,
+};
