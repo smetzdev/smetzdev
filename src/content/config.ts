4be51e6b329153e39dc4e0e910bc290d5d4756c8
Sascha Metz <hello@smetz.dev>
2024-08-18 22:17:49 +0200

    refactor(content): :sparkles: add notes

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,8 +11,19 @@ const projectCollection = defineCollection({
     designerLink: z.string().optional(),
   }),
 });
+
+const notesCollection = defineCollection({
+  type: "data",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    href: z.string(),
+  }),
+});
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   projects: projectCollection,
+  notes: notesCollection,
 };
