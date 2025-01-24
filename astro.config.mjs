ba06cedc825c19628ccc4c893b74c7227c082c24
Sascha Metz <hello@smetz.dev>
2025-01-24 17:05:13 +0100

    chore: :arrow_up: tailwind-v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   // ...
-  integrations: [tailwind({
-    // Example: Disable injecting a basic `base.css` import on every page.
-    // Useful if you need to define and/or import your own custom `base.css`.
-    applyBaseStyles: false
-  }), react(), alpinejs()]
+  integrations: [react(), alpinejs()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
